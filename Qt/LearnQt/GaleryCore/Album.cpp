d14dc7a507f77e42285f3675434d5d0f9fbc39ba
Rudnets_D <Rudnets_D@credo-dialogue.com>
2019-04-23 15:31:44 +0300

    - add new project - GaleryCore

---
+++ Qt/LearnQt/GaleryCore/Album.cpp
@@ -0,0 +1,42 @@
+﻿/// @file	Album.cpp
+///
+/// @see   Album.h
+///
+/// @author	RDN © Intern :)
+///
+/// @date   03.02.2019   15:44
+///
+
+#include "StdAfx.h"
+#include "GaleryCoreDecl.h"
+
+#include "Album.h"
+
+namespace gallery
+{
+	Album::Album(const QString& name /*= QString()*/)
+		: m_Name(name)
+		, m_Id(EntityIdType::InvalidId)
+	{
+	}
+
+	int Album::GetId() const
+	{
+		return m_Id;
+	}
+
+	void Album::SetId(int id)
+	{
+		m_Id = id;
+	}
+
+	QString const & Album::GetName() const
+	{
+		return m_Name;
+	}
+
+	void Album::SetName(const QString& name)
+	{
+		m_Name = name;
+	}
+}
---
+++ Qt/LearnQt/GaleryCore/Picture.cpp
@@ -0,0 +1,59 @@
+﻿/// @file	Picture.cpp
+///
+/// @see   Picture.h
+///
+/// @author	RDN © Intern :)
+///
+/// @date   21.02.2019   16:43
+///
+
+#include "StdAfx.h"
+#include "GaleryCoreDecl.h"
+#include "Picture.h"
+
+#include <QUrl>
+
+namespace gallery
+{
+	Picture::Picture(const QString & filePath /*= QString()*/)
+		: Picture(QUrl::fromLocalFile(filePath))
+	{
+	}
+
+	Picture::Picture(const QUrl & fileUrl)
+		: m_fileUrl(fileUrl)
+		, m_Id(EntityIdType::InvalidId)
+		, m_albumId(EntityIdType::InvalidId)
+	{
+	}
+
+	int Picture::GetId() const
+	{
+		return m_Id;
+	}
+
+	void Picture::SetId(int id)
+	{
+		m_Id = id;
+	}
+
+	int Picture::GetAlbumId() const
+	{
+		return m_albumId;
+	}
+
+	void Picture::SetAlbumId(int albumId)
+	{
+		m_albumId = albumId;
+	}
+
+	QUrl const & Picture::GetFileUrl() const
+	{
+		return m_fileUrl;
+	}
+
+	void Picture::SetFileUrl(const QUrl& fileUrl)
+	{
+		m_fileUrl = fileUrl;
+	}
+}
